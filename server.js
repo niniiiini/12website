fb1c60e6f8ae4c83e90fdecf1763282072f02967
s1410822010 <nini041313@gmail.com>
2022-01-13 16:44:21 +0800

    new

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ var List = DB.create("list.db");
 //     { imgSrc: "../img/水1.png", name: "水鬼", area: "全台各地", years: "未知" },
 //     { imgSrc: "../img/虎1.png", name: "虎姑婆", area: "客家聚落", years: "未知" },
 //     { imgSrc: "../img/林1.png", name: "林投姐", area: "台南", years: "清代" },
-//     { imgSrc: "../img/玉1.png", name: "水鬼", area: "全台各地", years: "未知" },
-//     { imgSrc: "../img/紅1.png", name: "虎姑婆", area: "客家聚落", years: "未知" },
-//     { imgSrc: "../img/魔1.png", name: "林投姐", area: "台南", years: "清代" }
+//     { imgSrc: "../img/玉1.png", name: "玉山小飛俠", area: "玉山南峰", years: "1990年" },
+//     { imgSrc: "../img/紅1.png", name: "紅衣小女孩", area: "台中市北屯區", years: "1998年" },
+//     { imgSrc: "../img/魔1.png", name: "魔神仔", area: "全台中低海拔山區", years: "未知" }
 // ])
 server.get("/list", function(req, res){
 
@@ -30,4 +30,34 @@ server.get("/list", function(req, res){
         result.splice(3);
         res.send(result);
     } )
+})
+//
+server.get("/contact", function(req, res){
+    console.log(req.query);
+    
+    res.redirect("/");
+})
+
+server.post("/contact_me", function(req, res){
+    console.log(req.body);
+    //check 
+    Contact.insert(req.body);
+    res.end()
+})
+
+server.post("/contact_file", function(req, res){
+     var form = formidable({maxFileSize:300*1024});
+     form.parse(req, function(err, fields, files){
+         if(err){
+             res.status(400).send({error: err.message})
+         }
+         else{
+            var uploadPath="Vue_Bootstrap/upload";
+             //move file to uploaded file path
+            fs.renameSync(files.file.filepath, uploadPath+"/"+files.file.originalFilename);
+             //write fields to db
+
+             res.end();
+         }
+     })
 })
